0d9ded2f56a40c75cbada84bd3c1e176cbe9c7c1
佐藤　和大 <kazuhsat@MBP-15JAC-671.local>
2015-11-19 20:21:41 +0900

    トランスポート例外クラス追加

---
+++ lib/Sugar/Solr/Transport/TransportException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sugar\Solr\Transport;
+
+use Sugar\Solr;
+
+/**
+ * トランスポート例外クラス
+ *
+ * @author kazuhsat <kazuhsat5@gmail.com>
+ */
+class TransportException extends Solr\Exception
+{
+    //
+}
